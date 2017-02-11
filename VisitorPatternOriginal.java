5a85b94c541fa4fd7feb7b7f694f03a1b38b8020
Zhou Xuexuan <joe627487136@hotmail.com>
2017-02-11 15:22:59 +0800

    fix_employee

--- VisitorPatternOriginal.java
+++ VisitorPatternOriginal.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 
 public class VisitorPatternOriginal {
 	public static void main(String[] args){
+		SUTD oneSUTD = new SUTD ();
 		SUTDVisitor visitor = new SUTDVisitor();
-		Employee epe = new Employee();
-		epe.accept(visitor);
-		Professor NOOB = new Professor("YOU ARE SUCH A NOOB!",666);
-		NOOB.accept(visitor);
+		ArrayList<Employee> epe = oneSUTD.getEmployee();
+		for (Employee o:epe){
+			o.accept(visitor);
+		}
 	}
 }
 
@@ -22,34 +23,21 @@ class SUTDVisitor implements Visitor {
 
 	@Override
 	public void visit(Employee employee) {
-		SUTD oneSUTD = new SUTD ();
-		ArrayList<Employee> employee1 = oneSUTD.getEmployee();
-		for (int i =0; i < employee1.size(); i++) {
-			if (employee1.get(i) instanceof Professor) {
-				System.out.println("Prof: " + ((Professor) employee1.get(i)).getName() + ((Professor) employee1.get(i)).getNo_of_publications());
-			}
-			if (employee1.get(i) instanceof AdminStuff) {
-				System.out.println("Prof: " + ((AdminStuff) employee1.get(i)).getName() + ((AdminStuff) employee1.get(i)).getEfficiency());
-			}
-			if (employee1.get(i) instanceof Student) {
-				System.out.println("Prof: " + ((Student) employee1.get(i)).getName() + ((Student) employee1.get(i)).getGPA());
-			}
-		}
 	}
 
 	@Override
 	public void visit(Professor professor) {
-		System.out.println("Go away Trash, check your employee not prof");
+		System.out.println("Prof: " + (professor.getName() +" "+ (professor.getNo_of_publications())));
 	}
 
 	@Override
 	public void visit(AdminStuff adminStuff) {
-		System.out.println("Go away Trash, check your employee not admin");
+		System.out.println("Admin: " + (adminStuff.getName() +" "+ (adminStuff.getEfficiency())));
 	}
 
 	@Override
 	public void visit(Student student) {
-		System.out.println("Go away Trash, check your employee not student");
+		System.out.println("Student: " + (student.getName() +" "+ (student.getGPA())));
 	}
 }
 
